87c0b656198decf0d24d61866139991523e421b0
grind-t <grindt.gd@gmail.com>
2025-05-30 00:10:42 +0300

    build: init package

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/index.ts"),
+      name: "Moex",
+    },
+  },
+  plugins: [dts({ rollupTypes: true })],
+});
